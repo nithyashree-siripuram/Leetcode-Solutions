dd1c66f3fd612dcdb9854533c5d8ebff9194e98f
Nithya Shree Siripuram <nithyashreesiripuram5@gmail.com>
2026-09-09 12:18:18 +0530

    Add Easy Java Solution for 1. Two Sum from LeetCode

--- LeetCode/Java/Easy/1. Two Sum/README.md
+++ LeetCode/Java/Easy/1. Two Sum/README.md
@@ -8,8 +8,8 @@
 Array, Hash Table
 
 ### 🚀 Performance
-- **Runtime:** 36 ms
-- **Memory:** 47.2 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Hash Map & Hashing/1. Two Sum/Solution.java
+++ LeetCode/Patterns/Hash Map & Hashing/1. Two Sum/Solution.java
@@ -1,16 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int arr[]=new int[2];
         int len=nums.length;
         for(int i=0;i<len;i++){
             for(int j=i+1;j<len;j++){
                 if(nums[i]+nums[j]==target){
-                    arr[0]=i;
-                    arr[1]=j;
-                    break;
+                    return new int[]{i,j};
                 }
             }
         }
-        return arr;
+        return new int[]{-1,-1};
     }
 }
