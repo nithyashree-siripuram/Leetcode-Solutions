aad03d4d814c8b5c415ac561dfac9c670e5a5fee
Nithya Shree Siripuram <nithyashreesiripuram5@gmail.com>
2026-09-12 15:56:55 +0530

    Add Medium Java Solution for 940. Fruit Into Baskets from LeetCode

---
+++ LeetCode/Patterns/Sliding Window/940. Fruit Into Baskets/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int len=fruits.length;
+        int freq[]=new int[10];
+        int s=0;
+        int ind=0;
+        for(int i=0;i<len;i++){
+            freq[fruits[i]]++;
+        }
+        for(int i=freq.length-1;i>=0;i--){
+            if(freq[i]!=0){
+                s=s+freq[i];
+                ind++;
+            }
+            if(ind==2){
+                break;
+            }
+        }
+        return s;
+    }
+}
