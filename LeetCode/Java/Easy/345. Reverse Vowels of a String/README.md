d3388131b04cab5dbd73024e00b6870b90c437b5
Nithya Shree Siripuram <nithyashreesiripuram5@gmail.com>
2026-09-12 09:54:46 +0530

    Add Easy Java Solution for 345. Reverse Vowels of a String from LeetCode

--- LeetCode/Java/Easy/345. Reverse Vowels of a String/README.md
+++ LeetCode/Java/Easy/345. Reverse Vowels of a String/README.md
@@ -8,8 +8,8 @@
 Two Pointers, String
 
 ### 🚀 Performance
-- **Runtime:** 2 ms
-- **Memory:** 46.6 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Java/Easy/345. Reverse Vowels of a String/Solution.java
+++ LeetCode/Java/Easy/345. Reverse Vowels of a String/Solution.java
@@ -1,31 +1,23 @@
 class Solution {
     public String reverseVowels(String s) {
-        char[] arr = s.toCharArray();
-        int j = arr.length - 1;
-        for (int i = 0; i < j; ) {
-            if (!(arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' ||
-                  arr[i] == 'o' || arr[i] == 'u' ||
-                  arr[i] == 'A' || arr[i] == 'E' || arr[i] == 'I' ||
-                  arr[i] == 'O' || arr[i] == 'U')) {
-
+        int len=s.length();
+        int j=len-1;
+        char[] ch=s.toCharArray();
+        for(int i=0;i<j;){
+            if(ch[i]!='a'&&ch[i]!='e' &&ch[i]!='i'&&ch[i]!='o'&&ch[i]!='u'&&ch[i]!='A'&&ch[i]!='E'&&ch[i]!='I'&&ch[i]!='O'&&ch[i]!='U'){
                 i++;
-            } else if (!(arr[j] == 'a' || arr[j] == 'e' || arr[j] == 'i' ||
-                         arr[j] == 'o' || arr[j] == 'u' ||
-                         arr[j] == 'A' || arr[j] == 'E' || arr[j] == 'I' ||
-                         arr[j] == 'O' || arr[j] == 'U')) {
-
+            }
+            else if(ch[j]!='a'&&ch[j]!='e'&&ch[j]!='i'&&ch[j]!='o'&&ch[j]!='u'&&ch[j]!='A'&&ch[j]!='E'&&ch[j]!='I'&&ch[j]!='O'&&ch[j]!='U'){
                 j--;
-            } else {
-
-                char temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-
+            }
+            else{
+                char temp=ch[i];
+                ch[i]=ch[j];
+                ch[j]=temp;
                 i++;
                 j--;
             }
         }
-
-        return new String(arr);
+        return new String(ch);
     }
 }
