344566ceb8ec88bf33a4608f0859ba24c5806706
Nithya Shree Siripuram <nithyashreesiripuram5@gmail.com>
2026-09-20 03:53:00 +0530

    Add Easy Java Solution for 136. Single Number from LeetCode

--- LeetCode/Patterns/Bit Manipulation/136. Single Number/README.md
+++ LeetCode/Patterns/Bit Manipulation/136. Single Number/README.md
@@ -8,8 +8,8 @@
 Array, Bit Manipulation
 
 ### 🚀 Performance
-- **Runtime:** N/A
-- **Memory:** N/A
+- **Runtime:** 12 ms
+- **Memory:** 47.2 MB
 
 ---
 
--- LeetCode/Patterns/Bit Manipulation/136. Single Number/Solution.java
+++ LeetCode/Patterns/Bit Manipulation/136. Single Number/Solution.java
@@ -1,15 +1,18 @@
-class Solution{
-    public int singleNumber(int[] nums){
-        int len=nums.length;
-        int freq[]=new int[9999999];
-        for(int i=0;i<len;i++){
-            freq[nums[i]]++;          
-        }
-        for(int i=0;i<freq.length;i++){
-            if(freq[i]==1){
-                return i;
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int i:nums){
+            if(set.contains(i)){
+                set.remove(i);
+            }
+            else{
+                set.add(i);
             }
         }
-        return -1;
+        int n=0;
+        for(int i:set){
+            n= i;
+        }
+        return n;
     }
 }
