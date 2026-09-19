801c5ef65fb8caf53134b25b925cea1e3079bc26
Nithya Shree Siripuram <nithyashreesiripuram5@gmail.com>
2026-09-19 10:25:24 +0530

    Add Easy Java Solution for 20. Valid Parentheses from LeetCode

--- LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.java
+++ LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.java
@@ -1,27 +1,14 @@
 class Solution {
     public boolean isValid(String s) {
-        int len=s.length();
-        Stack<Character> st=new Stack<>();
-        for(int i=0;i<len;i++){
-            char ch=s.charAt(i);
-            if(ch=='(' || ch=='{' || ch=='['){
-                st.push(ch);
-            }
-            else{
-                if(st.empty()){
-                    return false;
-                }
-                char top=st.peek();
-                if(ch==')' && top=='(' 
-                    ||ch=='}' && top=='{'
-                    ||ch==']' && top=='['){
-                        st.pop();
-                }
-                else{
-                    return false;
-                }
+        while(true){
+            String old=s;
+            s=s.replace("()","");
+            s=s.replace("[]","");
+            s=s.replace("{}","");
+            if(s.equals(old)){
+                break;
             }
         }
-         return st.isEmpty();
+        return s.length()==0;
     }
 }
