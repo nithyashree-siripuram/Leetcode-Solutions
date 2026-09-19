d992e9010006368e7fe53a9d37da368b58975ce7
Nithya Shree Siripuram <nithyashreesiripuram5@gmail.com>
2026-09-20 04:03:51 +0530

    Add Easy Java Solution for 169. Majority Element from LeetCode

--- LeetCode/Java/Easy/169. Majority Element/Solution.java
+++ LeetCode/Java/Easy/169. Majority Element/Solution.java
@@ -1,17 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int len=nums.length;
+        int c=0;
         int max=0;
+        int len=nums.length;
         for(int i=0;i<len;i++){
-            int c=0;
-            for(int j=i+1;j<len;j++){
+            c=0;
+            for(int j=0;j<len;j++){
                 if(nums[i]==nums[j]){
                     c++;
                 }
             }
-            if(c>=(len/2)) return nums[i];
+            if(c>max){
+                max=c;
+            }
         }
-        return 0;
-
+        return max;
     }
 }
--- LeetCode/Patterns/Hash Map & Hashing/169. Majority Element/README.md
+++ LeetCode/Patterns/Hash Map & Hashing/169. Majority Element/README.md
@@ -8,8 +8,8 @@
 Array, Hash Table, Divide and Conquer, Sorting, Counting, Boyer–Moore Majority Vote Algorithm
 
 ### 🚀 Performance
-- **Runtime:** 1084 ms
-- **Memory:** 55.6 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
